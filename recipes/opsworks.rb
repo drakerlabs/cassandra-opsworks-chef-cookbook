4c117201a227abe407b2f6ec8e9ae860e3825698
KARTHIKR <karthikr@WORKSTATION.(none)>
2015-06-02 18:40:02 +0530

    modified opsworks recipe to handle versions

--- recipes/opsworks.rb
+++ recipes/opsworks.rb
@@ -7,12 +7,27 @@ else
   include_recipe "cassandra-opsworks::datastax"
 end
 
-%w(cassandra.yaml cassandra-env.sh).each do |f|
-  template File.join(node["cassandra"]["conf_dir"], f) do
-    source "#{f}.erb"
+version=node[:package][:version].match(/_(.*)_/m)[1].strip[0...3]
+
+case version
+when "1.1"
+ config = "cassandra11.yaml"
+when "1.2", "2.0", "2.1"
+ config = "cassandra.yaml"
+end
+
+template File.join(node["cassandra"]["conf_dir"], cassandra.yaml) do
+    source "#{config}.erb"
+    owner node["cassandra"]["user"]
+    group node["cassandra"]["user"]
+    mode  0644
+end
+
+template File.join(node["cassandra"]["conf_dir"], cassandra-env.sh) do
+    source "cassandra-env.sh.erb"
     owner node["cassandra"]["user"]
     group node["cassandra"]["user"]
     mode  0644
     notifies :restart, resources(:service => "cassandra")
-  end
 end
+
