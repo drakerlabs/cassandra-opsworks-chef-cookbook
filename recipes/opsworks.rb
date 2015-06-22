f8f984e4a0821350f4e880bf19475e7eaf513231
KARTHIKR <karthikr@WORKSTATION.(none)>
2015-06-22 19:18:43 +0530

    modified opsworks recipe

--- recipes/opsworks.rb
+++ recipes/opsworks.rb
@@ -7,23 +7,33 @@ else
   include_recipe "cassandra-opsworks::datastax"
 end
 
-version=node[:package][:version].match(/_(.*)_/m)[1].strip[0...3]
+version=node[:cassandra][:package][:version].match(/_(.*)_/m)[1].strip[0...3]
 
 case version
 when "1.1"
  config = "cassandra11.yaml"
+ snitch = node[:cassandra11][:snitch]
+ authority = node[:cassandra11][:authority]
 when "1.2", "2.0", "2.1"
  config = "cassandra.yaml"
+ snitch = node[:cassandra][:snitch]
+ authority = node[:cassandra][:authorizer] 
+else
+ puts "Invalid version"
 end
 
-template File.join(node["cassandra"]["conf_dir"], cassandra.yaml) do
+template File.join(node["cassandra"]["conf_dir"], 'cassandra.yaml') do
     source "#{config}.erb"
     owner node["cassandra"]["user"]
     group node["cassandra"]["user"]
+    variables({
+             :snitch => "#{snitch}",
+             :authority => "#{authority}"
+            })
     mode  0644
 end
 
-template File.join(node["cassandra"]["conf_dir"], cassandra-env.sh) do
+template File.join(node["cassandra"]["conf_dir"], 'cassandra-env.sh') do
     source "cassandra-env.sh.erb"
     owner node["cassandra"]["user"]
     group node["cassandra"]["user"]
@@ -31,3 +41,25 @@ template File.join(node["cassandra"]["conf_dir"], cassandra-env.sh) do
     notifies :restart, resources(:service => "cassandra")
 end
 
+if version = "1.2"
+bash "comment_parameters_1_2" do
+   user "root"
+   code <<-EOF
+      sed -i 's/^cas_contention_timeout_in_ms/#cas_contention_timeout_in_ms/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+      sed -i 's/^preheat_kernel_page_cache/#preheat_kernel_page_cache/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+   EOF
+ end
+end
+
+if version = "2.1"
+bash "comment_parameters_2_1" do
+   user "root"
+   code <<-EOF
+      sed -i 's/^multithreaded_compaction/#multithreaded_compaction/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+      sed -i 's/^memtable_flush_queue_size/#memtable_flush_queue_size/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+      sed -i 's/^compaction_preheat_key_cache/#compaction_preheat_key_cache/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+      sed -i 's/^in_memory_compaction_limit_in_mb/#in_memory_compaction_limit_in_mb/' #{node["cassandra"]["conf_dir"]}/cassandra.yaml
+      sed -i 's/jamm.0.2.5.jar/jamm.0.3.0.jar/' #{node["cassandra"]["conf_dir"]}/cassandra-env.sh
+   EOF
+ end
+end
